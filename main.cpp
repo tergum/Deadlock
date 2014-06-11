0324f14eaaf3c85c09d8c27a968becdf244ad222
Sergii Targonskyi <odo1kemono@gmail.com>
2014-06-11 17:54:34 +0300

    Deadlock imitation primitive

--- main.cpp
+++ main.cpp
@@ -15,24 +15,26 @@ using std::endl;
 int main()
 {
     //Deadlock simulation
-    /*deadlock a(4), b(5);
+    deadlock a(4), b(5);
 
     std::thread extra_thread(&deadlock::operator<, &a, std::ref(b));
 
     b.operator<(a);
 
-    extra_thread.join();*/
+    extra_thread.join();
 
     //Non-deadlock
     Value v1(4), v2(56);
-    cout << "v1=" << v1 << ", v2=" << v2 << endl;
+/*    cout << "v1=" << v1 << ", v2=" << v2 << endl;
 
     swap(v1, v2);
 
     cout << "v1=" << v1 << ", v2=" << v2 << endl;
 
-
+*/
     cout << "Happy end!\n";
+
+    //
 
     return 0;
 }
